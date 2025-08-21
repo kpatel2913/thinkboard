6f0a2a4573f40fa683bc8eccae1d55e8fcde3c2f
Kris Patel <kpatel29@binghamton.edu>
2025-08-21 10:46:17 -0400

    Initial commit — ThinkBoard project (no secrets)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => toast.success("congrats")}>Click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
