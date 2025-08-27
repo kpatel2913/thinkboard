ba357a13b946ef43e5675fc75694c57701064b25
Kris Patel <kpatel29@binghamton.edu>
2025-08-27 11:28:27 -0400

    HomePage complete

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
@@ -9,6 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// middeware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
 
